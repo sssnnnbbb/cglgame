be1efffc58625a713fd3073bc851e57ee2d51191
SSSNNNBBB <shinobu.takemura@gmail.com>
2023-12-19 14:20:15 +0900

    Update main.js

--- docs/wallmaze/main.js
+++ docs/wallmaze/main.js
@@ -1,65 +1,83 @@
-title = "ミニマルメイズ";
+title = "壁の旅人";
 
 description = `
-[Hold] ジャンプ
+[Hold] 障害物を下に動かす
+左右キーで移動
 `;
 
-characters = [];
+characters = [
+  `
+  ll
+  l  l
+ llll
+l l  
+  lll
+ l 
+`
+];
 
 options = {
   isPlayingBgm: true,
   isReplayEnabled: true,
-  seed: 1,
-  theme: "simple",
+  seed: 3,
+  theme: "pixel",
 };
 
 /** @type {Vector} */
 let playerPos;
-/** @type {Vector} */
-let nextTarget;
+/** @type {{pos: Vector}[]} */
+let obstacles;
 /** @type {number} */
-let nextTargetDist;
-/** @type {boolean} */
-let isJumping;
-/** @type {number} */
-let jumpSpeed;
+let holdTime;
 
 function update() {
   if (!ticks) {
     playerPos = vec(50, 50);
-    nextTarget = vec(0, 0);
-    nextTargetDist = 0;
-    isJumping = false;
-    jumpSpeed = 1;
-  }
-  color("black");
-  char("a", playerPos);
-  
-  if (!isJumping) {
-    // ジャンプの開始
-    if (input.isJustPressed) {
-      play("jump");
-      nextTarget = vec(rnd(10, 90), rnd(10, 90));
-      nextTargetDist = playerPos.distanceTo(nextTarget);
-      isJumping = true;
-    }
-  } else {
-    // ジャンプ中の動き
-    const step = jumpSpeed * difficulty * 0.2;
-    playerPos.moveTowards(nextTarget, step);
-    color("light_blue");
-    line(playerPos, nextTarget, 2);
-    if (playerPos.distanceTo(nextTarget) < 2) {
-      isJumping = false;
-    }
+    obstacles = times(5, () => {
+      return {
+        pos: vec(rnd(10, 90), rnd(-90, 0))
+      };
+    });
+    holdTime = 0;
   }
 
-  // ゲームオーバーの条件
-  if (playerPos.x < 0 || playerPos.x > 100 || playerPos.y < 0 || playerPos.y > 100) {
+  // 壁の描画
+  color("light_blue");
+  rect(0, 0, 100, 100);
+
+  // プレイヤーの移動制御
+  if (input.isPressed) {
+    playerPos.x += input.pos.x > 50 ? 1 : -1;
+    playerPos.x = clamp(playerPos.x, 10, 90);
+    holdTime++;
+    addScore(1); // ホールドされた時間に基づいてスコアを加算
+  }
+
+  // 障害物の描画
+  color("purple");
+  obstacles.forEach((obstacle) => {
+    box(obstacle.pos, 5, 5);
+  });
+
+  // ホールドによる障害物の移動
+  if (input.isPressed) {
+    obstacles.forEach((obstacle) => {
+      obstacle.pos.y += 1;
+      if (obstacle.pos.y > 100) {
+        obstacle.pos.y = rnd(-90, 0);
+        obstacle.pos.x = rnd(10, 90);
+      }
+    });
+  }
+
+  // プレイヤーの描画
+  color("black");
+  char("a", playerPos);
+
+  // プレイヤーと障害物の衝突判定
+  const collision = char("a", playerPos).isColliding.rect.purple;
+  if (collision) {
     play("explosion");
     end();
   }
-
-  // スコアの加算
-  addScore(floor(nextTargetDist - playerPos.distanceTo(nextTarget)));
 }
